cb92c55d090e72bd467747af7c977069cb251b75
smonrou <saulnadremm@gmail.com>
2026-03-04 10:26:58 -0600

    first commit

---
+++ src/main/java/com/cunori/laboratio/SpringPeliculas/SpringPeliculasApplication.java
@@ -0,0 +1,13 @@
+package com.cunori.laboratio.SpringPeliculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPeliculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPeliculasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cunori/laboratio/SpringPeliculas/SpringPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cunori.laboratio.SpringPeliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
